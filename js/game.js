ddee04d384c771144f2fceacf9dda20c1fb70e2f
Jessy Hurbain <JessyHurbain@gmail.com>
2016-03-02 21:53:47 +0100

    Controlling the player with the keyboard

--- js/game.js
+++ js/game.js
@@ -12,8 +12,9 @@ function preload() {
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48)
 
 }
-
+var player;
 var platforms;
+var cursors;
 
 function create() {
   //  We're going to be using physics, so enable the Arcade Physics system
@@ -48,9 +49,12 @@ function create() {
   player.body.collideWorldBounds = true;
 
   //  Our two animations, walking left and right.
-  player.animations.add('left',[0, 1, 2, 3], 10, true);
-  player.animations.add('right',[5, 6, 7, 8], 10, true);
+  player.animations.add('left', [0, 1, 2, 3], 10, true);
+  player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+
+    //  Our controls.
+    cursors = game.input.keyboard.createCursorKeys();
 
 
 }
@@ -58,5 +62,30 @@ function create() {
 function update() {
   //  Collide the player and the stars with the platforms
   game.physics.arcade.collide(player, platforms);
+
+  //  Reset the players velocity (movement)
+  player.body.velocity.x = 0;
+
+  if (cursors.left.isDown) {
+    //  Move to the left
+    player.body.velocity.x = -150;
+
+    player.animations.play('left');
+  } else if (cursors.right.isDown) {
+    //  Move to the right
+    player.body.velocity.x = 150;
+
+    player.animations.play('right');
+  } else {
+    //  Stand still
+    player.animations.stop();
+
+    player.frame = 4;
+  }
+
+  //  Allow the player to jump if they are touching the ground.
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.body.velocity.y = -350;
+  }
 
 }
